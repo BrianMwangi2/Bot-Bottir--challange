1b688afd75260d1af15a077b82aa5590024cdb62
Brian <brian.mwangi2@student.moringaschool.com>
2024-05-05 21:09:26 +0300

    firt commit

---
+++ src/components/BotCollection.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './App.css';
+
+function BotCollection({ onAddBot, bots }) {
+  return (
+    <div>
+      <h1>My Bot Collection</h1>
+      <ul>
+        {bots.map((bot) => (
+          <li key={bot.id}>
+            <h2>{bot.name}</h2>
+            <img src={bot.avatar_url} alt={bot.name} />
+            <p>{bot.health}</p>
+            <p>{bot.damage}</p>
+            <p>{bot.armor}</p>
+            <p>{bot.bot_class}</p>
+            <p>{bot.created_at}</p>
+            <p>{bot.updated_at}</p>
+            <button onClick={() => onAddBot(bot)}>Add to Army</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default BotCollection;
---
+++ src/components/YourBotArmy.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './App.css';
+
+function YourBotArmy({ bots, onReleaseBot, onDischargeBot }) {
+  return (
+    <div>
+      <h2>Your Bot Army</h2>
+      <ul>
+        {bots.map((bot) => (
+          <li key={bot.id}>
+            <h2>{bot.name}</h2>
+            <img src={bot.avatar_url} alt={bot.name} />
+            <p>{bot.health}</p>
+            <p>{bot.damage}</p>
+            <p>{bot.armor}</p>
+            <p>{bot.bot_class}</p>
+            <p>{bot.created_at}</p>
+            <p>{bot.updated_at}</p>
+            <button onClick={() => onReleaseBot(bot)}>Release from Army</button>
+            <button onClick={() => onDischargeBot(bot)}>Discharge from Service</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default YourBotArmy;
